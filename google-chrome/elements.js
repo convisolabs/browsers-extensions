bd380301d6534dfab5c78b59cf26d445a7c8c214
Lucasmick1 <lucas_29_costa@hotmail.com>
2022-03-11 11:56:23 -0300

    Segregando variáveis

---
+++ google-chrome/elements.js
@@ -0,0 +1,5 @@
+const newVulnerabilityButton = document.querySelector("#btn-newvuln");
+const editVulnerabilityButton = document.querySelector("#btn-editvuln");
+const chatVulnerabilityButton = document.querySelector("#btn-chatvuln");
+const newProjectButton = document.querySelector("#btn-newproject");
+const chatProjectButton = document.querySelector("#btn-chatproject");
